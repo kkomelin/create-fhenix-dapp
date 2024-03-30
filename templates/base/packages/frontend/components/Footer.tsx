6ef1ef97a5754061a9aae0c42a4611e3bd9b9903
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-03-31 07:58:42 +0900

    Updated header and footer of the frontend app.

--- templates/base/packages/frontend/components/Footer.tsx
+++ templates/base/packages/frontend/components/Footer.tsx
@@ -1,8 +1,6 @@
-import React from "react";
+import { CurrencyDollarIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { hardhat } from "viem/chains";
-import { CurrencyDollarIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { HeartIcon } from "@heroicons/react/24/outline";
 import { SwitchTheme } from "~~/components/SwitchTheme";
 import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
 import { Faucet } from "~~/components/scaffold-eth";
@@ -46,16 +44,8 @@ export const Footer = () => {
       <div className="w-full">
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">
-            <div className="text-center">
-              <a href="https://github.com/scaffold-eth/se-2" target="_blank" rel="noreferrer" className="link">
-                Fork me
-              </a>
-            </div>
-            <span>·</span>
             <div className="flex justify-center items-center gap-2">
-              <p className="m-0 text-center">
-                Built with <HeartIcon className="inline-block h-4 w-4" /> at
-              </p>
+              <p className="m-0 text-center">Build with</p>
               <a
                 className="flex justify-center items-center gap-1"
                 href="https://buidlguidl.com/"
@@ -65,10 +55,19 @@ export const Footer = () => {
                 <BuidlGuidlLogo className="w-3 h-5 pb-1" />
                 <span className="link">BuidlGuidl</span>
               </a>
+              and
+              <a href="https://github.com/kkomelin/create-fhenix-dapp" target="_blank" rel="noreferrer">
+                <span className="link">Create Fhenix DApp</span>
+              </a>
             </div>
             <span>·</span>
             <div className="text-center">
-              <a href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA" target="_blank" rel="noreferrer" className="link">
+              <a
+                href="https://github.com/kkomelin/create-fhenix-dapp/issues"
+                target="_blank"
+                rel="noreferrer"
+                className="link"
+              >
                 Support
               </a>
             </div>
