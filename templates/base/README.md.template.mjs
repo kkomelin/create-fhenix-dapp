563d81ac6a67a24e5798daac09bede4327ea13af
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-04-10 16:55:42 +0700

    Updated readme templates.

--- templates/base/README.md.template.mjs
+++ templates/base/README.md.template.mjs
@@ -21,38 +21,50 @@ pnpm install
 
 ${
   Boolean(solidityFramework[0])
-    ? `2. Run a local network in the first terminal:
+    ? `2. Run a local network:
 
 \`\`\`
 pnpm chain
 \`\`\`
 
-This command starts a local Ethereum network using ${solidityFramework[0]}. The network runs on your local machine and can be used for testing and development. You can customize the network configuration in ${networkConfigPath[0]}.
+This command starts a local Fhenix node using ${solidityFramework[0]} and Docker for testing and development. 
+You can customize the network configuration in ${networkConfigPath[0]}.
 
-3. On a second terminal, deploy the test contract:
+3. Deploy a demo contract to the local network:
 
 \`\`\`
 pnpm deploy:contracts
 \`\`\`
 
-This command deploys a test smart contract to the local network. The contract is located in ${contractsPath[0]} and can be modified to suit your needs. The \`yarn deploy\` command uses the deploy script located in ${scriptsPath[0]} to deploy the contract to the network. You can also customize the deploy script.
+Note: The default deployer contract will be automatically funded to be able to deploy to the local node.
 
-4. On a third terminal, start your NextJS app:`
-    : "2. Start your NextJS app:"
+The contract can be modified here: ${contractsPath[0]}
+The contract deployment script can be customized here: ${scriptsPath[0]}
+
+4. (optional) Run smart contract tests to make sure it's been deployed successfully:
+
+\`\`\`
+pnpm test
+\`\`\`
+
+5. Then start your Next.js app:`
+    : "2. Start your Next.js app:"
 }
 
 \`\`\`
 pnpm start
 \`\`\`
 
-Visit your app on: \`http://localhost:3000\`. You can interact with your smart contract using the \`Debug Contracts\` page. 
+Visit your app on: \`http://localhost:3000\`. You can interact with your smart contract using the \`Debug Contracts\` page of the Counter demo on the frontpage. 
 You can tweak the app config in ${frontendScaffoldConfigPath[0]}.
 ${
   Boolean(solidityFramework[0])
     ? `
-Run smart contract test with ${testCommand[0]}
 
-- Edit your smart contract \`YourContract.sol\` in ${contractsPath[0]}
+## Development entry points
+
+- Tweak the app config in ${frontendScaffoldConfigPath[0]}.
+- Edit your smart contract \`Counter.sol\` in ${contractsPath[0]}.
 - Edit your frontend homepage at ${frontendAppPagePath[0]}. For guidance on [routing](https://nextjs.org/docs/app/building-your-application/routing/defining-routes) and configuring [pages/layouts](https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts) checkout the Next.js documentation.
 - Edit your deployment scripts in ${scriptsPath[0]}
 `
@@ -71,15 +83,16 @@ const contents = ({
   `# 🏗 Create Fhenix DApp
 
 <h4 align="center">
+  <a href="https://docs.fhenix.zone/docs/devdocs/intro">Fhenix Documentation</a> |
   <a href="https://docs.scaffoldeth.io">Documentation</a> |
   <a href="https://scaffoldeth.io">Website</a>
 </h4>
 
-🧪 An open-source, up-to-date toolkit for building decentralized applications (dapps) on the Ethereum blockchain. It's designed to make it easier for developers to create and deploy smart contracts and build user interfaces that interact with those contracts.
+🧪 An open-source, up-to-date toolkit for building decentralized applications (dapps) on the Fhenix blockchain. It's designed to make it easier for developers to create and deploy smart contracts and build user interfaces that interact with those contracts.
 
-⚙️ Built using NextJS, RainbowKit, ${
+⚙️ Built using Next.js, RainbowKit, ${
     Boolean(solidityFramework[0]) ? solidityFramework[0] + ", " : ""
-  }Wagmi, Viem, and Typescript.
+  }Wagmi, Viem,  Ethers.js, Fhenixjs and Typescript.
 ${
   Boolean(solidityFramework[0])
     ? "\n- ✅ **Contract Hot Reload**: Your frontend auto-adapts to your smart contract as you edit it."
@@ -89,6 +102,7 @@ ${
 - 🧱 [**Components**](https://docs.scaffoldeth.io/components/): Collection of common web3 components to quickly build your frontend.
 - 🔥 **Burner Wallet & Local Faucet**: Quickly test your application with a burner wallet and local faucet.
 - 🔐 **Integration with Wallet Providers**: Connect to different wallet providers and interact with the Ethereum network.
+- **Fhenix tools**: fhenix-hardhat-docker, fhenix-hardhat-plugin and fhenixjs for a quick start with Fhenix.
 
 ![Debug Contracts tab](https://github.com/scaffold-eth/scaffold-eth-2/assets/55535804/b237af0c-5027-4849-a5c1-2e31495cccb1)
 
@@ -97,8 +111,9 @@ ${
 Before you begin, you need to install the following tools:
 
 - [Node (>= v18.17)](https://nodejs.org/en/download/)
-- Yarn ([v1](https://classic.yarnpkg.com/en/docs/install/) or [v2+](https://yarnpkg.com/getting-started/install))
+- [pnpm](https://pnpm.io/installation)
 - [Git](https://git-scm.com/downloads)
+- [Docker](https://docs.docker.com/engine/install/)
 
 ${getQuickStart({
   solidityFramework,
@@ -111,15 +126,13 @@ ${getQuickStart({
 })}
 ## Documentation
 
-Visit our [docs](https://docs.scaffoldeth.io) to learn how to start building with Create Fhenix DApp.
+-  <a href="https://www.fhenix.io/">Fhenix Website</a>
+-  <a href="https://docs.fhenix.zone/docs/devdocs/intro">Fhenix Documentation</a>
+-  <a href="https://docs.scaffoldeth.io">Scaffold ETH Documentation</a>
 
-To know more about its features, check out our [website](https://scaffoldeth.io).
+## Credits
 
-## Contributing to Create Fhenix DApp
-
-We welcome contributions to Create Fhenix DApp!
-
-Please see [CONTRIBUTING.MD](https://github.com/scaffold-eth/scaffold-eth-2/blob/main/CONTRIBUTING.md) for more information and guidelines for contributing to Create Fhenix DApp.`;
+Many thanks to [Scaffold ETH](https://scaffoldeth.io/) developers for providing a solid foundation for this project.`;
 
 export default withDefaults(contents, {
   solidityFramework: "",
--- templates/extensions/hardhat/README.md.args.mjs
+++ templates/extensions/hardhat/README.md.args.mjs
@@ -4,4 +4,4 @@ export const contractsPath = `\`packages/backend/contracts\``;
 export const frontendScaffoldConfigPath = `\`packages/frontend/scaffold.config.ts\``;
 export const frontendAppPagePath = `\`packages/frontend/app/page.tsx\``;
 export const scriptsPath = `\`packages/backend/deploy\``;
-export const testCommand = `\`yarn hardhat:test\``;
+export const testCommand = `\`pnpm test\``;
